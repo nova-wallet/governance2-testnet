514529e20feb310c8fd49d5cb88ea6b47c3c55e4
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-11-11 12:27:23 +0500

    change logic for fast-runtime

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -248,14 +248,14 @@ impl sp_runtime::traits::Convert<sp_core::U256, Balance> for U256ToBalance {
 #[macro_export]
 macro_rules! prod_or_fast {
 	($prod:expr, $test:expr) => {
-		if cfg!(feature = "fast-runtime") {
+		if cfg!(feature = "std") {
 			$test
 		} else {
 			$prod
 		}
 	};
 	($prod:expr, $test:expr, $env:expr) => {
-		if cfg!(feature = "fast-runtime") {
+		if cfg!(feature = "std") {
 			core::option_env!($env).map(|s| s.parse().ok()).flatten().unwrap_or($test)
 		} else {
 			$prod
